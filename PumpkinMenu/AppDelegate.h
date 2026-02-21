f217f036c9f9045bbb83b72c5743c239d2009c93
Daniel Loffgren <daniel@loffgren.org>
2026-02-20 21:54:10 -0800

    Initial Commit

---
+++ PumpkinMenu/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PumpkinMenu
+//
+//  Created by Daniel Loffgren on 2/20/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ PumpkinMenu/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  PumpkinMenu
+//
+//  Created by Daniel Loffgren on 2/20/26.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
